279851f6f448d75c87062b5d70ac01c948994428
purav-parekh <purav98@gmail.com>
2025-08-25 11:54:45 -0400

    change nav bar placement

--- parsEPD/src/components/Navigation.tsx
+++ parsEPD/src/components/Navigation.tsx
@@ -4,15 +4,15 @@ import { LuExternalLink } from "react-icons/lu";
 const Nav = () => {
 	return (
 		<Box background="black" width="100%" padding={8} color="white">
-			<Flex gap="10" justifyContent={"center"} fontSize={"lg"} fontWeight={"bold"}>
+			<Flex gap="10" justifyContent={"flex-end"} fontSize={"lg"} fontWeight={"bold"}>
 				<Link href="../../public/nist-header-footer/nist-combined.css" target="_blank">
 					User Guide <LuExternalLink />
 				</Link>
+				<Link href="https://www.nist.gov/programs-projects/building-system-economics" target="_blank">
+					Building System Economics Project <LuExternalLink />
+				</Link>
 				<Link href="https://github.com/usnistgov/pdf-digitization/" target="_blank">
 					Github <LuExternalLink />
-				</Link>
-				<Link href="https://www.nist.gov/programs-projects/building-system-economics" target="_blank">
-					Building System Economics <LuExternalLink />
 				</Link>
 			</Flex>
 		</Box>
